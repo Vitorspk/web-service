6ff64c3be44e020f06aa38a49a54231a7b35b941
Vitor <vitorspk@gmail.com>
2017-01-23 19:22:49 -0200

    Update SupplierOM.java

--- purchase/SupplierOM.java
+++ purchase/SupplierOM.java
@@ -1,4 +1,4 @@
-package br.com.microdatasistemas.simintegrationws.xml.purchase;
+package xml.purchase;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
