57b07a0c0bf67728605a611d80113cbefa2dee15
Vitor <vitorspk@gmail.com>
2017-01-23 19:27:01 -0200

    Update MicrodataException.java

--- util/MicrodataException.java
+++ util/MicrodataException.java
@@ -1,17 +1,17 @@
-package br.com.microdatasistemas.simintegrationws.util;
+package util;
 
-public class MicrodataException extends Exception {
+public class Exception extends Exception {
     private static final long serialVersionUID = -6679173993840660479L;
     
-    public MicrodataException(Throwable e) {
+    public Exception(Throwable e) {
 	super(e);
     }
 
-    public MicrodataException(String message) {
+    public Exception(String message) {
 	super(message);
     }
     
-    public MicrodataException(String message, Throwable e) {
+    public Exception(String message, Throwable e) {
 	super(message, e);
     }
 }
