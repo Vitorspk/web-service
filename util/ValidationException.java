9737d562658ddc51fc670767f7cb942cf8fd1d72
Vitor <vitorspk@gmail.com>
2017-01-23 19:28:44 -0200

    Update ValidationException.java

--- util/ValidationException.java
+++ util/ValidationException.java
@@ -1,6 +1,6 @@
-package br.com.microdatasistemas.simintegrationws.util;
+package util;
 
-public class ValidationException extends MicrodataException {
+public class ValidationException extends Exception {
     private static final long serialVersionUID = 5429943754524887878L;
     
     public ValidationException(Throwable e) {
